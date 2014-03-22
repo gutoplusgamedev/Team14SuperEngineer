9a8f987bcb343e29dcc6aa6e062beb3356787759
Jose Augusto Thomas <joseaugustothomas@hotmail.com>
2014-03-21 17:47:17 -0700

    First commit

---
+++ Unity Project/SuperEngineer/Assets/Code.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Code 
+{
+	public List<CodePiece> pieces;
+}
---
+++ Unity Project/SuperEngineer/Assets/CodePiece.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CodePiece 
+{
+	public string item;
+}
