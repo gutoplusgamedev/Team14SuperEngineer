846d22912c78943bb42f4d1259bb9436d2fd462b
g9-KK <karlkong2004@hotmail.com>
2014-03-22 15:03:40 -0700

    can move character and can move camera with platforms

---
+++ Unity Project/SuperEngineer/Assets/CamControl.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+var lift : float  = 1.5;
+var distance : float = 10;
+var target: Transform;
+function Update () {
+transform.position = target.position - Vector3( 0, lift, distance);
+transform.LookAt (target);
+}
---
+++ Unity Project/SuperEngineer/Assets/PlayerControl.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+
+private var rotationSpeed : float = 100 ;
+private var moveSpeed : float = 7.3;
+private var jumpSpeed:	float = 10;
+private var downSpeed: float = -10;
+private var canJump = true;
+
+
+function Update () {
+var rotation: float = Input.GetAxis ("Vertical") * rotationSpeed;
+rotation *= Time.deltaTime;
+rigidbody.AddRelativeTorque (Vector3.back * rotation);
+
+rigidbody.velocity.x = moveSpeed;
+
+if (transform.position.y > 3) {
+canJump = false;
+}
+else {
+canJump = true;
+}
+
+if( Input.GetKeyDown(KeyCode.W) && canJump == true) {
+rigidbody.velocity.y = jumpSpeed;
+}
+if( Input.GetKeyDown(KeyCode.S) && canJump == true) {
+rigidbody.velocity.y = downSpeed;
+}
+
+
+
+}
