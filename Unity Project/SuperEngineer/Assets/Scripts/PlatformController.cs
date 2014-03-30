0dcdea24795417092bd5904abd3b1f52cf5e1312
Jose Augusto Thomas <joseaugustothomas@hotmail.com>
2014-03-30 05:24:26 -0700

    Added PlatformController to check whether the platform can be removed.

---
+++ Unity Project/SuperEngineer/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour 
+{
+	IEnumerator OnBecameInvisible ()
+	{
+		yield return new WaitForSeconds (1);
+		if (transform.renderer.isVisible) 
+		{
+			OnBecameInvisible ();
+		} 
+		else 
+		{
+			Destroy (gameObject);
+		}
+	}
+}
