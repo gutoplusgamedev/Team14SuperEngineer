dcfe753fd4b4c29bcbf7c21f9bfec7a7f518020c
g9-KK <karlkong2004@hotmail.com>
2014-03-29 19:49:38 -0700

    added textures

---
+++ Unity Project/SuperEngineer/Assets/playerLighting.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+var lift : float  = 1.5;
+var distance : float = 10;
+var target: Transform;
+
+function Update () {
+transform.position = target.position - Vector3( 0, -10,0);
+transform.LookAt (target);
+}
